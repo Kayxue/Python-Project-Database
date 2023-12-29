12696dd418cf68f48105c1947d7a16936c8002b5
Kayxue <s0990006@gmail.com>
2023-12-30 00:18:54 +0800

    2023/12/29 Add Post Order route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  public constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  public getHello(): string {
+    return 'Welcome to the api of Python-Project';
   }
 }
