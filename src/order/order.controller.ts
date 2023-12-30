682d575368dd58e8e312cac5628c2b0fe472dc71
Kayxue <s0990006@gmail.com>
2023-12-30 21:44:47 +0800

    2023/12/30 Add api things

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { PostOrderBody } from '../../Types/RequestTypes.dto';
-import { ApiBody, ApiQuery } from '@nestjs/swagger';
+import { ApiBody, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('order')
 export class OrderController {
@@ -25,11 +25,20 @@ export class OrderController {
   )
   @Post('post')
   @ApiBody({ type: PostOrderBody })
+  @ApiResponse({
+    status: 201,
+    description: 'An order has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Post body not complete or is empty',
+  })
   public async postOrder(@Body() body: PostOrderBody) {
     return this.orderService.postOrder(body);
   }
 
   @ApiQuery({ name: 'id', required: false })
+  @ApiResponse({ status: 200, description: 'Get data completed' })
   @Get('get')
   public async getOrder(@Query('id') orderId?: string) {
     return (await this.orderService.getOrder(orderId)) ?? {};
