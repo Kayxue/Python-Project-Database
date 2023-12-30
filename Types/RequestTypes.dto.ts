bd5f66e1d0435983874bc8c31c99080bb2c7be27
Kayxue <s0990006@gmail.com>
2023-12-30 21:23:36 +0800

    2023/12/30 Fixed Swagger UI DTO Type can't shown (maybe)

--- Types/RequestTypes.dto.ts
+++ Types/RequestTypes.dto.ts
@@ -1,27 +1,33 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsInt, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 
 export class ItemData{
+    @ApiProperty({type:Number})
     @IsInt()
     @IsNotEmpty()
     public readonly big:number
 
+    @ApiProperty({type:Number})
     @IsInt()
     @IsNotEmpty()
     public readonly medium:number
 }
 
 export class OrdersItem {
+    @ApiProperty({type:String})
     @IsString()
     @IsNotEmpty()
     public readonly name:string
     
+    @ApiProperty({type:ItemData})
     @ValidateNested()
     @IsNotEmpty()
     public readonly data:ItemData
 }
 
 export class PostOrderBody {
+    @ApiProperty({type:[OrdersItem]})
     @IsNotEmpty()
     @IsArray()
     public readonly orders: OrdersItem[];
