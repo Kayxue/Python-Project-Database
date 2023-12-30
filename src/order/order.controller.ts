09300cd6de9e3c4c848cbc66939936bcd84cce88
Kayxue <s0990006@gmail.com>
2023-12-30 11:15:21 +0800

    2023/12/29 Finished Post Order route

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
+  public constructor(private orderService: OrderService) {}
+
   @Post('post')
   public async postOrder(@Body() body) {
-    return body;
+    return this.orderService.postOrder(body);
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { OrderDetail } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  public constructor(private prismaService: PrismaService) {}
+
+  public async postOrder(data: {
+    orders: { name: string; data: { big: number; medium: number } }[];
+  }): Promise<OrderDetail> {
+    const result = await this.prismaService.orderDetail.create({
+      data: {
+        orders: {
+          create: data.orders.map((e) => {
+            return {
+              name: e.name,
+              data: {
+                create: {
+                  big: e.data.big,
+                  medium: e.data.medium,
+                },
+              },
+            };
+          }),
+        },
+      },
+      include: {
+        orders: {
+          include: {
+            data: true,
+          },
+        },
+      },
+    });
+
+    return result;
+  }
+}
