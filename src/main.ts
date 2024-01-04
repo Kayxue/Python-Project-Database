29b59c9a65e14a81fcff9d9fae43a177c31b5e5c
Kayxue <s0990006@gmail.com>
2024-01-04 20:10:21 +0800

    2024/1/4 Try to change swagger theme

--- src/main.ts
+++ src/main.ts
@@ -19,9 +19,7 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
     ],
     customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      'https://raw.githubusercontent.com/ostranme/swagger-ui-themes/develop/themes/3.x/theme-flattop.css',
     ],
   });
   await app.listen(3000);
